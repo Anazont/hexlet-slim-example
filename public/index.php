7156d8f19fcc03c6648e93c207ff5a25c481a0d2
Irek Gaysin <azpirin123z>
2021-04-15 17:07:27 +0300

    addTemplates

--- public/index.php
+++ public/index.php
@@ -3,9 +3,18 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
+
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория, в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
 
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
+
 
 $app->get('/', function ($request, $response) {
     $response->getBody()->write('Welcome to Slim!');
@@ -20,6 +29,21 @@ $app->get('/users', function ($request, $response) {
 
 $app->post('/users', function ($request, $response) {
     return $response->withStatus(302);
+});
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
+    // $this доступен внутри анонимной функции благодаря https://php.net/manual/ru/closure.bindto.php
+    // $this в Slim это контейнер зависимостей
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+});
+
+$app->get('/courses', function ($request, $response) use ($courses) {
+    $params = [
+        'courses' => $courses
+    ];
+    return $this->get('renderer')->render($response, 'courses/index.phtml', $params);
 });
 
 $app->run();
